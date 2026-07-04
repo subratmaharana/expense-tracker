ebcbb0824d3b585c4eb7ea5eba94ff1c83313743
subratmaharana <subrat.code05@gmail.com>
2026-07-04 17:19:45 +0530

    Create accounts and expenses apps and configure MySQL database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'accounts' ,
+    'expenses'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'expense_tracker',
+        'USER' : 'root' ,
+        'PASSWORD' : 'Subrat@2005' ,
+        'HOST' : 'localhost' ,
+        'PORT' :  '3306',
     }
 }
 
