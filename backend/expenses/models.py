e0ad1ca0d1ff319f3e3c27f2101d83f9ca7835c6
subratmaharana <subrat.code05@gmail.com>
2026-07-06 12:30:36 +0530

    Create Trasaction model

--- backend/expenses/models.py
+++ backend/expenses/models.py
@@ -6,3 +6,31 @@ class Category(models.Model) :
 
     def __str__(self):
         return self.name
+
+class Transaction(models.Model):
+
+    TRANSACTION_TYPES = [
+        ('Income', 'Income'),
+        ('Expense', 'Expense'),
+    ]
+
+    title = models.CharField(max_length=150)
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    transaction_type = models.CharField(
+        max_length=10,
+        choices=TRANSACTION_TYPES
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+
+    date = models.DateField()
+
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title        
