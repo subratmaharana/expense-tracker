38e46a87d1792f241387b6d159e07a7be351058c
subratmaharana <subrat.code05@gmail.com>
2026-07-07 11:41:21 +0530

    Create project template and static structure

--- backend/expenses/models.py
+++ backend/expenses/models.py
@@ -15,7 +15,7 @@ class Transaction(models.Model):
     ]
 
     title = models.CharField(max_length=150)
-
+ 
     amount = models.DecimalField(max_digits=10, decimal_places=2)
 
     transaction_type = models.CharField(
