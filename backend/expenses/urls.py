7acfb62ff02abf773577d5fce5b4922852e68e9d
subratmaharana <subrat.code05@gmail.com>
2026-07-12 18:58:55 +0530

    Implement category management interface

--- backend/expenses/urls.py
+++ backend/expenses/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("dashboard/", views.dashboard, name="dashboard"),
+    path("categories/", views.category_list, name="category_list"),
+    path("categories/add/", views.add_category, name="add_category"),
 ]
--- backend/expenses/views.py
+++ backend/expenses/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from .models import Category
+from django.shortcuts import render , redirect
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request) :
@@ -7,3 +8,24 @@ def home(request) :
 @login_required
 def dashboard(request):
     return render(request, "expenses/dashboard.html")
+
+@login_required
+def category_list(request):
+    categories = Category.objects.filter(user=request.user)
+    return render(request, "expenses/category_list.html", {
+        "categories": categories
+    })
+
+@login_required
+def add_category(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+
+        Category.objects.create(
+            user=request.user,
+            name=name
+        )
+
+        return redirect("category_list")
+
+    return render(request, "expenses/add_category.html")
