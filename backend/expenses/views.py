e418f6b4021c35de32f32d7a28ffcc56ba0281cb
subratmaharana <subrat.code05@gmail.com>
2026-07-13 23:08:24 +0530

    Implement add expense functionality

--- backend/expenses/views.py
+++ backend/expenses/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.contrib.auth.decorators import login_required
-from .models import Category
+from .models import Category , Transaction
 
 
 def home(request):
@@ -19,3 +19,42 @@ def view_category(request):
     return render(request, "expenses/view_categories.html", {
         "categories": categories
     })
+
+@login_required
+def add_expense(request):
+
+    categories = Category.objects.all()
+
+    if request.method == "POST":
+
+        category = Category.objects.get(
+            id=request.POST.get("category")
+        )
+
+        Transaction.objects.create(
+
+            user=request.user,
+
+            title=request.POST.get("title"),
+
+            amount=request.POST.get("amount"),
+
+            transaction_type=request.POST.get("transaction_type"),
+
+            category=category,
+
+            date=request.POST.get("date"),
+
+            description=request.POST.get("description")
+
+        )
+
+        return redirect("dashboard")
+
+    return render(
+        request,
+        "expenses/add_expense.html",
+        {
+            "categories": categories
+        }
+    )
